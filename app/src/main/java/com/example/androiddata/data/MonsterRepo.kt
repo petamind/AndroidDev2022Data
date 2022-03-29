63b63e922407278a5fe57c39a4a4050f1b60ad6c
tung <tungsd@gmail.com>
2022-03-29 16:06:33 +1300

    Fix moshi retrofit adapter with KotlinJsonAdapterFactory

--- app/src/main/java/com/example/androiddata/data/MonsterRepo.kt
+++ app/src/main/java/com/example/androiddata/data/MonsterRepo.kt
@@ -1,17 +1,11 @@
 package com.example.androiddata.data
 
 import android.app.Application
-import android.util.Log
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.MutableLiveData
-import com.example.androiddata.R
-import com.example.androiddata.utils.FileHelper
-import com.example.androiddata.utils.LOG_TAG
 import com.example.androiddata.utils.NetworkHelper
 import com.example.androiddata.utils.WEB_SERVICE_URL
-import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,9 +16,6 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 class MonsterRepo(private val app: Application) {
 
     val monsterData = MutableLiveData<List<Monster>>()
-
-
-    private val listTypes = Types.newParameterizedType(List::class.java, Monster::class.java)
 
     init {
         CoroutineScope(Dispatchers.IO).launch {
@@ -40,9 +31,6 @@ class MonsterRepo(private val app: Application) {
                 .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().add(KotlinJsonAdapterFactory()).build()))
                 .build()
             val service = retrofit.create(MonsterService::class.java)
-            val test = service.getMonsterData().also {
-                Log.i(LOG_TAG, "callWebService: ${it.body()}")
-            }
             val serviceData = service.getMonsterData().body() ?: emptyList()
             monsterData.postValue(serviceData)
         }
